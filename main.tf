afde68389ac00854321b47709385f920effcb124
deeks <you@example.com>
2024-08-03 00:37:02 +0200

    updated bucket acl

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
-  profile = "defualt"
+  region  = "us-east-1"
+  profile = "default"  # Corrected typo from "defualt" to "default"
 }
 
 resource "aws_s3_bucket" "example" {
   bucket = "my-example-bucket-${random_id.random.prefix}"
+}
+
+resource "aws_s3_bucket_acl" "example_acl" {
+  bucket = aws_s3_bucket.example.id
   acl    = "private"
 }
 
