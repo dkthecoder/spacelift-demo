a847c572f3e2bfecb258801921e266e458c20221
dkthecoder <53783341+dkthecoder@users.noreply.github.com>
2024-08-02 22:31:26 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-example-bucket-${random_id.random.prefix}"
+  acl    = "private"
+}
+
+resource "random_id" "random" {
+  byte_length = 4
+}
