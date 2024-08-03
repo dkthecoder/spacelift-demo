aeab6f3f691d19116153a6703397c8765367ed4e
deeks <you@example.com>
2024-08-03 03:49:24 +0200

    removed acl for bucket

--- main.tf
+++ main.tf
@@ -6,9 +6,4 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-example-bucket-${random_uuid.uuid.result}"
 }
 
-resource "aws_s3_bucket_acl" "example_acl" {
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
-}
-
 resource "random_uuid" "uuid" {}
